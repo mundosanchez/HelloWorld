28e91a4f4a90204707bb7dc4ac23e22d55a5732b
Mundo <Mundo@DESKTOP-92GVFEN>
2021-03-21 08:17:41 -0400

    First commit

---
+++ HelloWorld/src/hello_world/HelloWorld.java
@@ -0,0 +1,14 @@
+package hello_world;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for (i=0;i<10;i++)
+		{
+		System.out.println("Hello World!");
+		}
+
+	}
+}
